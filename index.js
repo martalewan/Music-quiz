f835c1e69ffad69a78d81f3c263d18202750091e
leonardo-nunez <lnpb@hotmail.com>
2021-11-27 14:37:23 +0100

    serving react from node

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// const http = require('http');
+const express = require('express');
 const app = require('express')();
+const path = require('path');
 require('dotenv').config();
-const socketsConnection = require('./sockets')
 
-const testRoute = require('./routes/test');
+const socketsConnection = require('./server/sockets')
+const testRoute = require('./server/routes/test');
 
 const PORT = process.env.PORT || 9090;
 const HOST = process.env.HOST || 'http://localhost:';
@@ -11,6 +12,12 @@ const HOST = process.env.HOST || 'http://localhost:';
 const server = app.listen(PORT, () => console.log(`Listening on port ${HOST}${PORT}`));
 
 socketsConnection(server);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use('/test', testRoute);
 
