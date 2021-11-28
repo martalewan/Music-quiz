4748d938ed74d507557754efd2dba4dd5d3bb971
leonardo-nunez <lnpb@hotmail.com>
2021-11-28 14:35:01 +0100

    changing to location

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,20 @@ import { useEffect } from 'react';
 import survivor from './music/survivor.mp3';
 
 const App = () => {
-  const getEnvVars = async () => {
-    const vars = await fetch('/setup-vars/env-url').catch(err => console.err(err));
-    const { HOST, PORT } = await vars.json();
+  useEffect(() => {
+    const getEnvVars = async () => {
+      const HOST = window.location.origin.replace(/^http/, 'ws');
+      const socket = new WebSocket(HOST);
 
-    const socket = new WebSocket(`ws://${HOST}${PORT}`);
-
-    socket.addEventListener('message', e => {
-      console.log('BANANANANANA', e.data);
-    });
-    socket.addEventListener('open', () => {
-      console.log('Frontend is open for papi');
-    });
-  };
-  getEnvVars();
+      socket.addEventListener('message', e => {
+        console.log('BANANANANANA', e.data);
+      });
+      socket.addEventListener('open', () => {
+        console.log('Frontend is open for papi');
+      });
+    };
+    getEnvVars();
+  }, []);
 
   return (
     <div className="App">
