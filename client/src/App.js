0996376160ca9ff78df476c9dfe4ce75d509cce8
MatteoCinti <matteocinti@hotmail.com>
2021-11-28 13:59:46 +0100

    resetting env vars

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import { useEffect } from 'react';
 import survivor from './music/survivor.mp3';
 
 const App = () => {
-  useEffect(() => {
-    const socket = new WebSocket('ws://localhost:9999');
+  const getEnvVars = async () => {
+    const vars = await fetch('/setup-vars/env-url').catch(err => console.err(err));
+    const { HOST, PORT } = await vars.json();
+
+    const socket = new WebSocket(`ws://${HOST}${PORT}`);
+
     socket.addEventListener('message', e => {
       console.log('BANANANANANA', e.data);
     });
     socket.addEventListener('open', () => {
       console.log('Frontend is open for papi');
     });
-  }, []);
+  };
+  getEnvVars();
 
   return (
     <div className="App">
